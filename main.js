8189f649fd7d6108acdc6da7c4eb69d4250e7fa9
ninja076786 <77653028+ninja076786@users.noreply.github.com>
2021-07-22 17:59:25 -0700

    Update main.js

--- main.js
+++ main.js
@@ -22,3 +22,21 @@ Webcam.set({
     console.log("ml5 version:",ml5.version)
      
     classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/63kKwjJsT/model.json",modelLoaded)
+    function modelLoaded(){
+        console.log("Model Has Loaded!")
+         }
+    function analyze_image(){
+        img = document.getElementById("captured_img");
+
+        classifier.classify(img, got_result);
+    }
+
+    function got_result(error, results){
+       if(error){
+           console.error(error);
+       }else{
+       console.log(results);
+       document.getElementById(object_name).innerHTML=results[0].label
+       document.getElementById(acurracy).innerHTML=results[0].confidence.toFixed(3)
+       }
+    }
